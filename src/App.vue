2579956b47850c078427edf656b82fd34c236747
sumon <md.daudulislamsumon@gmail.com>
2021-05-12 22:52:09 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div id="page-top">
+      <my-header />
+      <my-intro-section />
+      <my-main-section />
+      <my-footer />
+      <router-view />
+    </div>
+  </div>
+</template>
+<script>
+import Header from '@/components/includes/Header.vue';
+import IntroSection from '@/views/IntroSection.vue';
+import MainSection from '@/views/MainSection.vue';
+import Footer from '@/components/includes/Footer.vue';
+export default {
+  name: 'app',
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {
+    'my-header': Header,
+    'my-intro-section': IntroSection,
+    'my-main-section': MainSection,
+    'my-footer': Footer
+  },
+  created() {},
+  mounted() {}
+};
+</script>
+<style></style>
---
+++ src/views/MainSection.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <main id="main">
+      <my-about-section />
+      <my-services-section />
+      <my-counter-sectin />
+      <my-portfolio-section />
+      <my-testimonial-section />
+      <my-blog-section />
+      <my-contact-section />
+    </main>
+  </div>
+</template>
+
+<script>
+import AboutSection from '@/components/AboutSection.vue';
+import ServicesSection from '@/components/ServicesSection.vue';
+import CounterSection from '@/components/CounterSection.vue';
+import PortfolioSection from '@/components/PortfolioSection.vue';
+import TestimonialsSection from '@/components/TestimonialsSection.vue';
+import BlogSection from '@/components/BlogSection.vue';
+import ContactSection from '@/components/ContactSection.vue';
+export default {
+  name: 'MainSection',
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {
+    'my-about-section': AboutSection,
+    'my-services-section': ServicesSection,
+    'my-counter-sectin': CounterSection,
+    'my-portfolio-section': PortfolioSection,
+    'my-testimonial-section': TestimonialsSection,
+    'my-blog-section': BlogSection,
+    'my-contact-section': ContactSection
+  },
+  created() {},
+  mounted() {}
+};
+</script>
+
+<style></style>
